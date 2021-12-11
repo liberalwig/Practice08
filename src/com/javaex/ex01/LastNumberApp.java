80359bfc0ba707a0c13f1d79f2da945b0f88324e
최승은 <liberalfilm@gmail.com>
2021-12-11 16:42:34 +0900

    2021.12.11(토)16:41 try-catch, int[]속성 알기

--- src/com/javaex/ex01/LastNumberApp.java
+++ src/com/javaex/ex01/LastNumberApp.java
@@ -1,3 +1,11 @@
+/*Done/2021.12.10(금), 2021.12.11(토)16:43: try-catch, int[]속성 알기
+
+[문제] com.javaex.ex01 패키지
+LastNumberApp.java 코드 실행시 아래와 같이 예외가 발생합니다.
+lastValue() 메소드를 수정해서 아래와 같이 출력되도록 하세요.(main() 메소드는 수정하지 않습니다.)
+
+*/
+
 package com.javaex.ex01;
 
 public class LastNumberApp {
@@ -8,21 +16,22 @@ public class LastNumberApp {
 		lastValue();
 	}
 
-
-	//런타임시 오류발생
+	// 런타임시 오류발생
 	public static void lastValue() {
 
-		int[] intArray = new int[3];
-		intArray[0] = 3;
-		intArray[1] = 6;
-		intArray[2] = 9;
-
-		int result;
-		
 		// 아래부분을 try~catch문을 사용하여 수정하세요
-		result = intArray[3];
-		System.out.println(result);
-		
+
+		int[] intArray = new int[] { 3, 6, 9 };
+		int result;
+
+		try {
+			result = intArray[3];
+			System.out.println(result);
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("배열의 범위를 넘었습니다.");
+		}
+
 	}
 
 }
