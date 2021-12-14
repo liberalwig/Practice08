6410f9f4fbd26367c6646b6432ed926d1adfcf84
최승은 <liberalfilm@gmail.com>
2021-12-14 10:26:27 +0900

    2021.12.14(화)10:26 미_

--- src/com/javaex/ex05/Add.java
+++ src/com/javaex/ex05/Add.java
@@ -1,14 +1,22 @@
+/*2021.12.14(화)10:13
+
+[문제] com.javaex.ex04 패키지
+아래와 같이 숫자를 입력받고 출력되도록 프로그램을 작성하세요.
+
+*/
+
 package com.javaex.ex05;
 
 public class Add {
-    
-    private int a;
-    private int b;
-    
-    public void setValue(int a, int b) {
-    }
 
-    public int calculate() {
-    }
+	private int a;
+	private int b;
+
+	public void setValue(int a, int b) {
+	}
+
+	public int calculate() {
+		return a + b;
+	}
 
 }
--- src/com/javaex/ex05/CalcApp.java
+++ src/com/javaex/ex05/CalcApp.java
@@ -1,8 +1,34 @@
+/*2021.12.14(화)10:13-10:25
+
+[문제] com.javaex.ex04 패키지
+아래와 같이 숫자를 입력받고 출력되도록 프로그램을 작성하세요.
+
+*/
+
 package com.javaex.ex05;
+
+import java.util.Scanner;
 
 public class CalcApp {
 
     public static void main(String[] args) {
 
+    	Scanner sc = new Scannner(System.in);
+    	
+    	 
+    	
+    	
+    	
+    	sc.close();
     }
 }
+
+/*
+2021.12.14(화)10:05
+네 개의 연산자를 제외한 &같은 게 나오면 =>println알수없는연산
+"\q"가 나오면 =>println 종료합니다
+
+네 연산자가 아닌 모든 연산자 등장 경우의 수를 어떻게 제외할 것인가.
+if로 한다고 하여도 어떻게 제외시키지.
+
+*/
